793fbf95723796bb6b6b8351d8514b4952bbcc79
bodgan-medvedev <bodgan.medvedev@gmail.com>
2017-07-23 22:52:57 +0300

    get_testnet_eth add faucet get testcoin

---
+++ app/api/ropsten.js
@@ -0,0 +1,46 @@
+
+var request = require('../modules/request');
+
+module.exports = (API, redis) => {
+
+    API.on('get_testnet_eth', (user, param, callback) => {
+        let option = {
+            rejectUnauthorized: false,
+            host: 'ropsten.faucet.b9lab.com',
+            port: 443,
+            path: '/tap',
+            method: 'POST',
+            formData: "&toWhom="+param.address,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        };
+        request.getJSON(option, (statusCode, response) => {
+            callback && callback(null, {response:response,statusCode:statusCode,address:param.address});
+        });
+    }, {
+        title: 'Get eth testnet coin ropsten',
+        description: 'Get eth testnet coin ropsten (1eth) limit: 1 req in 1 min and (max 4 eth for  1 address)',
+        param: [
+            {
+                name: 'address',
+                type: "string",
+                title: 'address',
+                necessarily: false,
+                default: '0x'
+            }
+
+        ],
+        response: [
+            {name: 'success', type: "string", title: 'Success ?', default: 'true, false'},
+            {
+                name: 'txHash',
+                type: "string",
+                title: 'HASH transaction blockchain',
+                default: '0x*******'
+            },
+            {name: 'error', type: "object", title: '', default: 'ERROR'},
+            {name: 'latency_ms', type: "int(11)", title: 'Processing time of the request in ms', default: '122'}
+        ]
+    });
+};
--- test.js
+++ test.js
@@ -2,14 +2,20 @@
 const Web3 = require('web3');
 const web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://52.178.151.150:8545'));
-const Tx = require('ethereumjs-tx');
-const util = require('ethereumjs-util');
-const md5 = require('md5');
-
-const _ = require('lodash');
-const SolidityFunction = require('web3/lib/web3/function');
-console.log(web3.toHex({photo1:'https://gfdshsg.gsdg/gsdfgsdgdsfgfsdg.png'}));
-
+console.log(web3.toHex({fdsgd:'ggfd',fdsg:{"server": {
+    "http": {
+        "port": 8000
+    },
+    "https": {
+        "port": 8001
+    },
+    "ws": {
+        "port": 8002
+    },
+    "url": {
+        "path": "/"
+    }
+}}}));
 /**
  * Created by bogdanmedvedev on 23.07.17.
  */
