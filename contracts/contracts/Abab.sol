5463b6d20af844c2533e9662f0fe34ed0fc6c2af
Alexey Nebotov <alexey@nebotov.com>
2017-07-28 15:05:25 +0300

    add schedule unit tests

--- contracts/contracts/Abab.sol
+++ contracts/contracts/Abab.sol
@@ -12,23 +12,24 @@ contract Abab {
 
     struct Room {
         uint160 roomDescriptionHash;
-        uint schedulesLength;
+		address partner;
+		uint    partnerPPM;
+        uint    schedulesLength;
         mapping(uint => Schedule) schedules;
-        //uint[] schedulesFrom;  // storage! -don't worry  https://github.com/ethereum/solidity/issues/2549
     }
 	
-	mapping (address => Room[]) public rooms;
+	mapping (address => Room[]) public rooms;	
 
-	event NewRoom    (address indexed from, uint roomIndex, uint160 _roomDescriptionHash);
-	event UpdateRoom (address indexed from, uint roomIndex, uint160 _newRoomDescriptionHash);
-	event DeleteRoom (address indexed from, uint roomIndex);	
+	event NewRoom    (address indexed host, uint roomIndex, uint160 _roomDescriptionHash);
+	event UpdateRoom (address indexed host, uint roomIndex, uint160 _newRoomDescriptionHash);
+	event DeleteRoom (address indexed host, uint roomIndex);	
 	
-	function UpsertRoom(uint _roomIndex, uint160 _roomDescriptionHash)
+	function UpsertRoom(uint _roomIndex, uint160 _roomDescriptionHash, address partner, uint partnerPPM)
     public
 	returns (uint roomIndex)
 	{
 	    if(_roomIndex>=rooms[msg.sender].length) {
-			var newRoomIndex = rooms[msg.sender].push(Room(_roomDescriptionHash, 0))-1;
+			var newRoomIndex = rooms[msg.sender].push(Room(_roomDescriptionHash, partner, partnerPPM, 0))-1;
 			NewRoom(msg.sender, newRoomIndex, _roomDescriptionHash);
 			return newRoomIndex;
 		}
@@ -60,24 +61,35 @@ contract Abab {
             rooms[msg.sender][i] = rooms[msg.sender][i+1];
         
         rooms[msg.sender].length--;
+		
+		DeleteRoom(msg.sender, _roomIndex);
 	}
 
+	event NewSchedule    (address indexed host, uint roomIndex, uint from);
+	event UpdateSchedule (address indexed host, uint roomIndex, uint from);
+	event DeleteSchedule (address indexed host, uint roomIndex, uint from);	
+	
 	function UpsertSchedule(uint _roomIndex, uint _from, uint _to, uint _dayPrice, uint _weekPrice, uint _monthPrice) 
 	public
 	{
 	    var schedule = Schedule(_from, _to, _dayPrice, _weekPrice, _monthPrice);
 	    
+        var room = rooms[msg.sender][_roomIndex];
+        var schedulesLength = room.schedulesLength;
+
 	    //exist?
-	    for(uint i=0;i<rooms[msg.sender][_roomIndex].schedulesLength;++i)
-	        if(rooms[msg.sender][_roomIndex].schedules[i].from == _from) {
+	    for(uint i=0;i<schedulesLength;++i)
+	        if(room.schedules[i].from == _from) {
 	            // update
-	            rooms[msg.sender][_roomIndex].schedules[i] = schedule;
+	            room.schedules[i] = schedule;
+	            UpdateSchedule (msg.sender, _roomIndex, _from);
 	            return;
 	        }
 
         //insert
-		rooms[msg.sender][_roomIndex].schedules[_from] = schedule;
-        rooms[msg.sender][_roomIndex].schedulesLength++;
+		room.schedules[schedulesLength] = schedule;
+        room.schedulesLength = schedulesLength + 1;
+        NewSchedule(msg.sender, _roomIndex, _from);
 	}
 	
 	uint constant maxUInt = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
@@ -96,6 +108,9 @@ contract Abab {
 	public constant 
 	returns(uint length) 
 	{
+	    var addressRooms = rooms[msg.sender];
+	    if (addressRooms.length <= _roomIndex) 
+	        return 0;
 	    return rooms[msg.sender][_roomIndex].schedulesLength;
     }
 
@@ -107,19 +122,48 @@ contract Abab {
 	    return (s.from, s.to, s.dayPrice, s.weekPrice, s.monthPrice);
 	}
 	
+	function test() 
+	public
+	returns(uint from) 
+	{
+	    UpsertRoom(0,0,0,0);
+
+        uint _roomIndex = 0;
+        uint _from = 1;
+        uint _to = 1;
+        uint _dayPrice = 1;
+        uint _weekPrice = 1;
+        uint _monthPrice = 1;
+
+	    var schedule = Schedule(_from, _to, _dayPrice, _weekPrice, _monthPrice);
+	    
+        var room = rooms[msg.sender][_roomIndex];
+
+		room.schedules[0] = schedule;
+        room.schedulesLength++;
+
+
+	   // return rooms[msg.sender][0].schedules[0].from = 1;
+	    return rooms[msg.sender][0].schedules[0].from;
+	}
+	
+	
 	function RemoveSchedule(uint _roomIndex, uint _from)
 	public
 	{
+	    var length = rooms[msg.sender][_roomIndex].schedulesLength;
 	    uint index = maxUInt;
-	    for(uint i=0; i<rooms[msg.sender][_roomIndex].schedulesLength; ++i)
+	    for(uint i=0; i<length; ++i)
 	        if(rooms[msg.sender][_roomIndex].schedules[i].from == _from) 
 	            index = i;
 	    
-	    if (index == maxUInt) return;
+	    if (index == maxUInt) return; // not found
 	    
-        for (; index<rooms[msg.sender][_roomIndex].schedulesLength-1; index++)
+	    length = length-1;
+        for (; index<length; index++)
             rooms[msg.sender][_roomIndex].schedules[index] = rooms[msg.sender][_roomIndex].schedules[index+1];
         
-        rooms[msg.sender][_roomIndex].schedulesLength--;
+        rooms[msg.sender][_roomIndex].schedulesLength = length;
+	    DeleteSchedule(msg.sender,_roomIndex, _from);
 	}
 }
